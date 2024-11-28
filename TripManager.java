68d6315f1b2dd6351ab3b2d65fc6cc766878633a
denmill632 <denaemiller12@gmail.com>
2024-11-28 16:45:14 -0500

    Update TripManager.java

--- TripManager.java
+++ TripManager.java
@@ -1,1 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripManager {
+    private List<Trip> trips;
+
+    public TripManager() {
+        this.trips = new ArrayList<>();
+    }
+
+    // Add a new trip to the list
+    public void addTrip(Trip trip) {
+        trips.add(trip);
+    }
+
+    // Get all trips
+    public List<Trip> getTrips() {
+        return trips;
+    }
+
+    // Save or update an itinerary
+    public void saveItinerary(Trip trip) {
+        System.out.println("Saving itinerary: " + trip.getDestination());
+    }
+
+    public Trip getItinerary(String destination) {
+        for (Trip trip : trips) {
+            if (trip.getDestination().equalsIgnoreCase(destination)) {
+                return trip;
+            }
+        }
+        return null; // If no trip is found
+    }
+}
 
