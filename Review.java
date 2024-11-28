3cc336501a3dd662c58d52b7f2ba08cb7135cd87
uwimonagitnerd <deanchristopherjr@gmail.com>
2024-11-28 17:12:11 -0500

    Update Review.java

--- Review.java
+++ Review.java
@@ -1,19 +1,21 @@
 public class Review
 {
     private String reviewer;
-    private String review;
     private String location;
+    private String review;
+    private double rating;
 
-    public Review(String reviewer, String location, String review)
+    public Review(String reviewer, String location, String review, double rating)
     {
         this.reviewer = reviewer;
         this.location = location;
         this.review = review;
+        setRating(rating);
     }
 
     public String getReviewer()
     {
-        return review;
+        return reviewer;
     }
 
     public String getReview()
@@ -26,12 +28,36 @@ public class Review
         return location;
     }
 
+    public double getRating()
+    {
+        return rating;
+    }
+
+    public void setRating(double rating)
+    {
+        this.rating = 0;
+
+        if (rating > 5)
+        {
+            this.rating = 5;
+        }
+        else if (rating < 1)
+        {
+            this.rating = 1;
+        }
+        else
+        {
+            this.rating = rating;
+        }
+    }
+
     @Override
     public String toString()
     {
         return "Review:\n" +
             "Reviewer: " + reviewer + "\n" +
             "Location: " + location + "\n" +
-            "Review: " + review + "\n";
+            "Review: " + review + "\n" +
+            "Rating: " + rating + "\n";
     }
 }
